565c95409ac1f91f17c0f8d97a692a429fff42b7
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-06-30 14:32:59 -0400

    Updated Registration to Database

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -8,6 +8,7 @@ import ActivityPage from "../ActivityPage/ActivityPage"
 import NutritionPage from "../NutritionPage/NutritionPage"
 import NotFound from "../NotFound/NotFound"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {useState} from "react"
 import {AuthContextProvider, useAuthContext} from "../../contexts/auth"
 
 //  export default function AppContainer()
@@ -20,6 +21,15 @@ import {AuthContextProvider, useAuthContext} from "../../contexts/auth"
 //  }
 
 export default function App() {
+  const [form, setForm] = useState({
+    email: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    passwordConfirm: ""
+  })
+
   return (
     <div className="app">
       <React.Fragment>
@@ -29,7 +39,7 @@ export default function App() {
                 <Routes>
                       <Route path="/" element={<Landing />}></Route>
                       <Route path="/login" element={<LoginPage />}></Route>
-                      <Route path="/register" element={<RegistrationPage />}></Route>
+                      <Route path="/register" element={<RegistrationPage form={form} setForm={setForm}/>}></Route>
                       <Route path="/activity" element={<ActivityPage />}></Route>
                       <Route path="/nutrition/*" element={<NutritionPage />}></Route>
                       <Route path="*" element={<NotFound />}></Route>
--- lifetracker-ui/src/components/RegistrationPage/RegistrationForm.jsx
+++ lifetracker-ui/src/components/RegistrationPage/RegistrationForm.jsx
@@ -1,6 +1,38 @@
 import * as React from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
-export default function RegistrationForm() {
+export default function RegistrationForm(props) {       
+  //VARIABLES
+  const navigate = useNavigate()
+
+  //FUNCTION FOR WHEN A USER IS INPUTTING INFORMATION
+  function handleOnChange(evt)
+  {
+       props.setForm((formInput) => ({...formInput, [evt.target.name]: evt.target.value}))
+  }
+
+  //FUNCTION FOR WHEN A USER SUBMITS FORM
+  async function handleOnSubmit(evt)
+  {
+       evt.preventDefault();
+       const response = await axios.post("http://localhost:3001/auth/register", {
+              email: props.form.email,
+              username: props.form.username,
+              firstName: props.form.firstName,
+              lastName: props.form.lastName,
+              password: props.form.password,
+       })
+       .then
+       (
+              navigate("/login")
+       )
+       .catch((error) => {
+              console.log(error)
+       })
+       
+  }
+
   return (
     <div className="registration-form">
         <h1>Register</h1>
@@ -10,45 +42,45 @@ export default function RegistrationForm() {
                name="email"
                type="email"
                placeholder="Enter email ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <h3>Username</h3>
         <input className="form-input"
                name="username"
                type="text"
                placeholder="Enter username ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <h3>First Name</h3>
         <input className="form-input"
                name="firstName"
                type="text"
                placeholder="Enter first name ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <h3>Last Name</h3>
         <input className="form-input"
                name="lastName"
                type="text"
                placeholder="Enter last name ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <h3>Password</h3>
         <input className="form-input"
                name="password"
-               type="text"
+               type="password"
                placeholder="Enter password ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <h3>Password Confirm</h3>
         <input className="form-input"
                name="passwordConfirm"
-               type="text"
+               type="password"
                placeholder="Confirm password ..."
-               onChange = ""></input>
+               onChange = {(evt) => handleOnChange(evt)}></input>
 
         <br></br>
-        <button className="submit-registration">Create Account</button>
+        <button className="submit-registration" onClick={(evt) => handleOnSubmit(evt)}>Create Account</button>
     </div>
   )
 }
