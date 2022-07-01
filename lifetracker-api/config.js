8bd801c6b083fc82dfbf9c5b00f5a66a07dc8a06
Godmiliut <supermiliut72@gmail.com>
2022-07-01 01:36:38 -0400

    Connected to PSQL

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -5,13 +5,13 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
 
 function getDatabaseUri() {
     const dbUser = process.env.DATABASE_USER || "postgres"
-    const dbPass = process.env.DATABASE_PASS  ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+    const dbPass = process.env.DATABASE_PASS  ? encodeURI(process.env.DATABASE_PASS) : "Dazbabowind1"
     const dbHost = process.env.DATABASE_HOST || "localhost"
     const dbPort = process.env.DATABASE_PORT || 5432
     const dbName = process.env.DATABASE_NAME || "lifetracker"
-}
 
-return process.env.DATABE.URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+}
 
 const BCRYPT_WORK_FACTOR = 13
 
