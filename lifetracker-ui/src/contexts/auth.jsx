13c1aa1b4071dc52a73ad2e9591a06dcfa7ec773
aileen-ji <aileenji@umich.edu>
2022-07-07 13:09:01 -0400

    added auth.jsx

--- lifetracker-ui/src/contexts/auth.jsx
+++ lifetracker-ui/src/contexts/auth.jsx
@@ -1,4 +1,17 @@
 import * as React from "react"
-import { createContext } from "react";
+import { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext(null)
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState({})
+    const authValue = {user, setUser}
+
+    return (
+        <AuthContext.Provider value={authValue}>
+            <>{children}</>
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuthContext = () => useContext(AuthContext)
