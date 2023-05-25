e63a8d111107a87db765427e88a215d5ffdddda8
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-05-24 20:18:24 -0400

    update starter

--- lifetracker-api/server.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const app = require("./app")
-const { PORT } = require("./config")
-
-app.listen(PORT, function () {
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-})
--- lifetracker-ui/src/components/Tile/Tile.jsx
+++
@@ -1,9 +0,0 @@
-import "./Tile.css"
-
-export default function Tile(props) {
-  return (
-    <div className="Tile" {...props}>
-      <>{props.children}</>
-    </div>
-  )
-}
