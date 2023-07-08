590f6970c0ec784466736a22fe20327effeb4fbe
Vanessa Ibe <vibe@vibe-ltmtd24.internal.salesforce.com>
2023-07-07 21:13:53 -0700

    updated config

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -14,8 +14,8 @@ function getDatabaseUri() {
   const dbPort = process.env.PG_PORT || 5432
   const dbProdName = process.env.PG_DATABASE || "lifetracker"
 
-//   return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbProdName}`
-return "postgres://lifetracker_database_vibe_user:okKJMWJTdnrspn5c2HnJ7J7V8s0ar9Tz@dpg-cikd13h5rnuvtgv58pl0-a/lifetracker_database_vibe"
+return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbProdName}`
+//return "postgres://lifetracker_database_vibe_user:okKJMWJTdnrspn5c2HnJ7J7V8s0ar9Tz@dpg-cikd13h5rnuvtgv58pl0-a/lifetracker_database_vibe"
 }
 
 const BCRYPT_WORK_FACTOR = 13
