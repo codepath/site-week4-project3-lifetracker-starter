3f9b0507a8bd375d45c84c066c272fbb34fc8eb9
Oluwapelumi Tayo Orisadare <ptayoorisadare@ptayoor-ltm7f2y.internal.salesforce.com>
2023-07-09 22:12:03 -0700

    updating database.js file

--- lifetracker-api/database.js
+++ lifetracker-api/database.js
@@ -1,12 +1,21 @@
-require('dotenv').config()
-const { Pool } = require('pg')
+// require('dotenv').config()
+const { Pool } = require("pg");
+const { getDatabaseUri } = require("./config");
 
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-})
+const db = new Pool({
+  connectionString: getDatabaseUri(),
+});
 
-module.exports = pool;
+// user: process.env.PG_USER,
+// host: process.env.PG_HOST,
+// database: process.env.PG_DATABASE,
+// password: process.env.PG_PASSWORD,
+// port: process.env.PG_PORT
+db.connect((err) => {
+  if (err) console.error(("connectiong error", err));
+  else {
+    console.log("Successfully connected to postgres db!");
+  }
+});
+
+module.exports = db;
